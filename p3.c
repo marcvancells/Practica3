bc2a8108e4d9bcdd7a2407d2a1d5d2699c9095b7
Marc Vancells <vancellsmarc@gmail.com>
2023-11-20 10:11:04 +0100

    Ex 7

--- p3.c
+++ p3.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	int c[4];
-	int a[4] = {1,2,3,4};
-	int b[4] = {5,6,7,8};
-	for (int i=0;i<3;i++) {
-		c[i]=a[i] * b[i];
-	}
+ int i, a[20];
+ for( i = 0; i < 20; i++ ){
+ a[i] = i*4;
+ }
+ return 0;
 }
-
