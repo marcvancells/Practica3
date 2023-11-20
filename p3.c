ffee2d0f11491d928323f71713edd8a1ab283712
Marc Vancells <vancellsmarc@gmail.com>
2023-11-20 09:30:43 +0100

    Ex 4

--- p3.c
+++ p3.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	int a;
-	int b = 2;
-	int c = 3;
-	int d = 4;
-	a = b+c*d;
-	a = (b+c)*d;
-	d = a;
+	char a;
+	int b = 3;
+	int res;
+	res = 3*a;
 	return 0;
 }	
 
